48aa5e55a6d7f95de4c184abafbaa3ca4ddf7732
Chris_Home <antlantisgate@gmail.com>
2012-04-30 16:37:06 +0200

    function YAML::parseModel() added

--- src/FileParsers/YamlBodyJoint.cpp
+++ src/FileParsers/YamlBodyJoint.cpp
@@ -4,19 +4,37 @@ using namespace std;
 
 YamlBodyJoint::YamlBodyJoint(std::string fileName)
 {
-	std::ifstream file(fileName.c_str());
+	mFileName = fileName;
+	
+	/*std::ifstream file(fileName.c_str());
 	
 	YAML::Parser parser(file);
 	YAML::Node doc;
 	
 	parser.GetNextDocument(doc);
 	
-	doc[0];
+	const YAML::Node& node = doc["BJoints"];
+	const YAML::Node& node2 = node[0];
+	
+	string tempo;
+	node2["Parent"] >> tempo;
+	cout <<  tempo << endl;
 	std::cout << doc.size() << endl;
 	
-	file.close();
+	file.close();*/
 }
 
 YamlBodyJoint::~YamlBodyJoint()
 {
 }
+
+void YamlBodyJoint::parseModel()
+{
+	std::ifstream file(mFileName.c_str());
+	YAML::Parser parser(file);
+	YAML::Node node;
+	parser.GetNextDocument(node);
+	
+	file.close();
+}
+
--- src/FileParsers/YamlBodyJoint.h
+++ src/FileParsers/YamlBodyJoint.h
@@ -2,6 +2,7 @@
 #define YAMLBODYJOINT_H
 
 #include <yaml-cpp/yaml.h>
+#include "../3DModel/Joint.h"
 #include <fstream>
 #include <string>
 
@@ -10,6 +11,31 @@ class YamlBodyJoint
 	public:
 		YamlBodyJoint(std::string fileName);
 		~YamlBodyJoint();
+		
+		void parseModel();
+	
+	private:
+		typedef struct SBJoints
+		{
+			std::string Joint;
+			std::string Parent;
+		}SBJoints;
+			
+		typedef struct SYmdFile
+		{
+			std::string NbPoses;
+			std::string NbJoints;
+			std::string FirstIndex;
+			SBJoints BJoints;
+		}SYmdFile;
+		
+		
+		std::string mFileName;
+		Joint *mModel;
+		SYmdFile mParsedFile;
+		
+		
+
 };
 
 #endif
