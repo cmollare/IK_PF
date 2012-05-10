20d70f40e848242f28876c393c1946df8d8ac98b
Chris_Laptop <antlantisgate@gmail.com>
2012-05-10 10:27:05 +0200

    Doc of YamlBodyJoint file written

--- src/FileParsers/YamlBodyJoint.h
+++ src/FileParsers/YamlBodyJoint.h
@@ -1,52 +1,128 @@
 #ifndef YAMLBODYJOINT_H
 #define YAMLBODYJOINT_H
 
+/*!
+ * \file YamlBodyJoint.h
+ */
+
 #include <yaml-cpp/yaml.h>
 #include "../3DModel/Joint.h"
 #include <fstream>
 #include <string>
 
+/*!
+ * \struct SBJoints
+ * \brief Structure reprensenting a Joint
+ * 
+ * This structure contains all the informations for a Joint extracted from the ymd file.
+ * 
+ */
 typedef struct SBJoints
 {
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-	std::string Joint;
-	std::string Parent;
-	vector<float> Offset;
-	Eigen::Quaternionf Orientation;
+	std::string Joint; /*!< Name of the Joint */
+	std::string Parent; /*!< Name of the parent = "_Root" if this Joint is the root */ 
+	vector<float> Offset; /*!< Position of the Joint relative to its parent */
+	Eigen::Quaternionf Orientation; /*!< Local orientation of the Joint */
 }SBJoints;
 			
+/*!
+ * \struct SYmdFile
+ * \brief Structure representing the ymd file
+ */
 typedef struct SYmdFile
 {
 	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-	std::string NbPoses;
-	std::string NbJoints;
-	std::string FirstIndex;
-	vector<SBJoints, Eigen::aligned_allocator<SBJoints> > BJoints;
+	std::string NbPoses; /*!< Nb of poses in the ymd file (for mixed format) */
+	std::string NbJoints; /*!< Nb of Joints in the ymd file */
+	std::string FirstIndex; /*!< First index (not implemented yet) */
+	vector<SBJoints, Eigen::aligned_allocator<SBJoints> > BJoints; /*!< vector of SBJoints structure : contains all Joints of the ymd file */
 }SYmdFile;
+
+/*!
+ * \class YamlBodyJoint
+ * \brief Class for Yaml type file parse
+ * 
+ * This class parse the .ymd file and create a hierarchical model with the Joint class.
+ * 
+ */
 
 class YamlBodyJoint
 {
 	public:
+	
+		/*!
+		 * \fn YamlBodyJoint(std::string fileName)
+		 * \brief YamlBodyJoint constructor
+		 * \param fileName Path to the .ymd file.
+		 */
 		YamlBodyJoint(std::string fileName);
+		
+		/*!
+		 * \fn ~YamlBodyJoint()
+		 * \brief YamlBodyJoint destructor
+		 */
 		~YamlBodyJoint();
 		
+		/*!
+		 * \fn void parseModel()
+		 * \brief Parse the .ymd file
+		 */
 		void parseModel();
+		
+		/*!
+		 * \fn void createModel()
+		 * \brief Create the hierarchical structure with Joint class
+		 */
 		void createModel();
+		
+		/*!
+		 * \fn Joint* getModel()
+		 * \brief Return the model and call createModel, if the model is not created
+		 * \return A pointer on the root Joint.
+		 */
 		Joint* getModel();
 	
 	private:
 		
-		std::string mFileName;
-		Joint *mModel;
-		SYmdFile mParsedFile;
+		std::string mFileName; /*!< Name of the .ymd file */
+		Joint *mModel; /*!< Pointer on the root Joint */
+		SYmdFile mParsedFile; /*!< Structure of the parsed .ymd file */
 		
 		
 
 };
 
-void operator>> (const YAML::Node& node, SYmdFile &YmdFile);
-void operator>> (const YAML::Node& node, vector<SBJoints, Eigen::aligned_allocator<SBJoints> > &BJoints);
+/*!
+ * \fn void operator>> (const YAML::Node& node, SYmdFile& YmdFile)
+ * \brief Extract a SYmdFile structure from the corresponding node
+ * \param node Node to extract from.
+ * \param YmdFile Where to store the result.
+ */
+void operator>> (const YAML::Node& node, SYmdFile& YmdFile);
+
+/*!
+ * \fn void operator>> (const YAML::Node& node, vector<SBJoints, Eigen::aligned_allocator<SBJoints> >& BJoints)
+ * \brief Extract a vector of SBJoints structures from the corresponding node
+ * \param node Node to extract from.
+ * \param BJoints Where to store the result.
+ */
+void operator>> (const YAML::Node& node, vector<SBJoints, Eigen::aligned_allocator<SBJoints> >& BJoints);
+
+/*!
+ * \fn void operator>> (const YAML::Node& node, Eigen::Quaternionf& quat)
+ * \brief Extract a quaternion from the corresponding node
+ * \param node Node to extract from.
+ * \param quat Where to store the result.
+ */
 void operator>> (const YAML::Node& node, Eigen::Quaternionf& quat);
+
+/*!
+ * \fn void operator>> (const YAML::Node& node, vector<float>& Offset)
+ * \brief Extract a position vector<float> from the corresponding node
+ * \param node Node to extract from.
+ * \param Offset Where to store the result.
+ */
 void operator>> (const YAML::Node& node, vector<float>& Offset);
 
 #endif
