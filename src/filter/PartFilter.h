a6fb92b3fd386e688f53b7399ebcb084f7a8f03d
Chris_Laas <antlantisgate@gmail.com>
2012-05-30 11:28:24 +0200

    filter : adding of PartFilter class

---
+++ src/filter/PartFilter.h
@@ -0,0 +1,29 @@
+#ifndef PARTFILTER_H
+#define PARTFILTER_H
+
+#include "Filter.h"
+
+#define TEMP 0.2
+
+class PartFilter : public Filter
+{
+	public:
+		PartFilter(std::vector<S3DModel*> mods, std::vector<std::string> posNames, std::vector<std::vector<double> > jointsXYZPositions);
+		
+		virtual void initFilter();
+		virtual void computeLikelihood();
+		virtual void step(std::vector<std::vector<double> > frame);
+		double computeNeff();
+		void mapJointToObs(std::map<std::string, std::string> jointNameToPosName);
+		S3DModel* computeMMSE();
+		
+	protected:
+		virtual void computeDistance();
+		void updateWeights();
+		void resample();
+		
+		Eigen::VectorXf mCurrentWeights;
+		int mMaxWeightIndex;
+};
+
+#endif
